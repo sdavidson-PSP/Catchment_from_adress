74036ff086c6af78c9998f21440fdae376c8f238
sdavidson-PSP <53909933+sdavidson-PSP@users.noreply.github.com>
2019-11-25 10:32:02 -0500

    Making a different catchment check

---
+++ find_School_catchment.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+# Run in the acadonda3 cd .\Anaconda3\
+# comand in terminal is ./python "Path.py"
+
+
+# Imports
+import pandas as pd 
+import requests
+import time
+from bs4 import BeautifulSoup 
+
+
+
+# Load Data
+applicants = pd.read_csv("C:\Users\Sam\Anaconda3\Schools for catchment.csv")
+
+
+matched_records = 0
+total_records = len(applicants)
+
+print(f"Processing {len(applicants)} records.")
+# Look up addresses in database to get lat/lon
+for idx, row in applicants.iterrows():
+    if True:
+        coords = (row['latitude'], row['longitude'])
+        timestamp = int(time.time())
+        url = f"https://webapps1.philasd.org/school_finder/ajax/pip/{coords[0]}/{coords[1]}/{timestamp}"
+        print(f"Found address. Pinging {url}")
+    
+        # Call school finder to pull catchment name
+        response = requests.get(url)
+        if response.ok:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            if soup.h3:
+                catchment = soup.h3.contents[0]
+                print(f"Found catchment: {catchment}")
+                # Add catchment name to applicant data
+                applicants.loc[idx,'Catchment_Name'] = catchment
+                matched_records = matched_records + 1
+                print(f"Matched {matched_records} of {total_records}")
+
+# Save application/catchment data
+applicants.to_csv("Schools_with_catchments.csv", index=False)
+    
